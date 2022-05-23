d21af82c81e53c16564ba76a6fe35ba43f05fa05
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-05-23 16:00:32 +0200

    Solved conflicts and changed file structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import "./assets/App.scss";
+
+import JoinPage from "./pages/JoinPage";
+import GamePage from "./pages/GamePage";
+
+import useGetShips from "../src/hooks/useGetShips";
+import { useEffect } from "react";
+
+function App() {
+  const myBoard = useGetShips();
+
+  useEffect(() => {
+    myBoard.placeAllShips();
+  }, []);
+
+  console.table(myBoard.battleboard);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<JoinPage />} />
+        <Route path="/battleships" element={<GamePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
